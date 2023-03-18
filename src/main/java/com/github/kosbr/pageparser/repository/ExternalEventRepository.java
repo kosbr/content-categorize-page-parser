8ee84b4c4970cc4a0a4eb7315e73e96ebe8bb9a1
Konstantin Bryazgin <konstantin.bryazgin@diabolocom.com>
2023-03-18 22:25:58 +0200

    add more logs

--- src/main/java/com/github/kosbr/pageparser/repository/ExternalEventRepository.java
+++ src/main/java/com/github/kosbr/pageparser/repository/ExternalEventRepository.java
@@ -2,6 +2,8 @@ package com.github.kosbr.pageparser.repository;
 
 import com.github.kosbr.pageparser.event.CategorizeEvent;
 import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.messaging.support.MessageBuilder;
@@ -9,6 +11,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ExternalEventRepository {
+
+    private final static Logger LOG = LoggerFactory.getLogger(ExternalEventRepository.class);
+
 
     @Value("${configuration.matcher-exchange}")
     private String matcherExchange;
@@ -22,5 +27,6 @@ public class ExternalEventRepository {
     public void sendEventToCategorize(UUID taskId) {
         CategorizeEvent event = new CategorizeEvent(taskId);
         streamBridge.send(matcherExchange, MessageBuilder.withPayload(event).build());
+        LOG.info("Event sent to matcher " + taskId);
     }
 }
